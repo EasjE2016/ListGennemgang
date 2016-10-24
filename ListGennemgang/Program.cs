9b7dacaadd3cdcd0b9405c5fe4561c85548a5c65
Martin Kierkegaard <mark@easj.dk>
2016-10-24 22:24:17 +0200

    liste over hvad der skal gennemgåes

--- ListGennemgang/Program.cs
+++ ListGennemgang/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+//using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +10,40 @@ namespace ListGennemgang
     {
         static void Main(string[] args)
         {
+
+            //List<T> er en generisk liste. T betyder at her kan der stå en vilkårlig type fx int string eller en type i selv 
+            //har kodet fx et Person objekt eller Dice objekt.
+
+            //Nr 1 : en ny liste med heltal (int)
+
+            //tilføj tal //Add()
+
+            //Skriv antallet af elementer ud //liste.Count
+
+            //fjern element på bestemt position //RemoveAt()
+
+            //Skriv antallet af elementer ud //liste.Count
+
+            //Slet alt data i listen //Clear()
+
+
+            //Nr 2: en ny liste med strings
+
+
+            //Nr 3: en ny liste med objekter af fx studerende i 1R
+            //vi opretter en ny klasse Person
+
+            //tilføj ny variabel med Person
+
+            //tilføj objekt med Add()
+
+            //tilføj direkte med Add(new Person)
+
+            //foreach og tilføj objekt
+
+            //Spørgsmål ?
+
+
         }
     }
 }
